824952e413fa624222e35c8cc04a16585574dc17
MBatman <allancotrimt@gmail.com>
2014-03-24 23:56:19 -0400

    Cat2 alterações

--- projeto_aps/application/views/categoria2_view.php
+++ projeto_aps/application/views/categoria2_view.php
@@ -6,6 +6,7 @@
 		  <form method="post" action="categoria/cadastrar2">
 		    <label for="name">Nome</label>
 		    <input type="text" name="nomeCategoria" id="nomeCategoria" >
+                    
 		  <input type="submit" value="Próximo"/>
 		  </form>
 	</body>
